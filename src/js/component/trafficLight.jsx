c1bf7956303447879ca0164129d04272bef0782c
Jose Hurtado <hello@hurtadojose.com>
2022-07-07 03:09:57 +0000

    First Solution for Traffic Light

---
+++ src/js/component/trafficLight.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+
+export const TrafficLight = () => {
+	const [color, setColor] = useState("red");
+	useEffect(
+		() => {
+			const interval = setInterval(() => {
+				color === "green"
+					? setColor("red")
+					: color === "yellow"
+						? setColor("green")
+						: color === "red"
+							? setColor("yellow")
+							: null;
+			}, 2000);
+			return () => clearInterval(interval);
+		},
+		[color]
+	);
+
+	return (
+		<div className="body">
+			<div className="trafficPole container">
+			<div className="protector"></div>
+			<div className="protector"></div>
+			<div className="protector"></div>
+				<div
+					className={color === "red" ? "redOn" : "redOff"}
+					onClick={() => setColor("red")}
+				/>
+				<div
+					className={color === "yellow" ? "yellowOn" : "yellowOff"}
+					onClick={() => setColor("yellow")}
+				/>
+				<div
+					className={color === "green" ? "greenOn" : "greenOff"}
+					onClick={() => setColor("green")}
+				/>
+			</div>
+		</div>
+	);
+};
